66297f460a19c6853534b2d36ba806f5e71a99c4
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2025-03-03 22:19:55 -0500

    fix: log the error in generate manifest

--- cmd/kubeit/commands/generate_manifest.go
+++ cmd/kubeit/commands/generate_manifest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/komailo/kubeit/internal/logger"
 	"github.com/komailo/kubeit/pkg/generate"
 	"github.com/spf13/cobra"
 )
@@ -40,6 +41,7 @@ var GenerateManifestCmd = &cobra.Command{
 			}
 			finalErr := fmt.Errorf("\n%s", strings.Join(formattedErrors, "\n"))
 			cmd.SetContext(context.WithValue(cmd.Context(), cmdErrorKey, finalErr))
+			logger.Errorf("Error generating manifests: %v", finalErr)
 		}
 
 	},
