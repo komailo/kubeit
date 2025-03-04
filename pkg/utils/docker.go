30820456cdce89c77d9e3e086817dd080341f79f
Komail Kanjee <1796298+komailo@users.noreply.github.com>
2025-03-04 01:40:22 -0500

    Generate manifest from docker image (#15)
    This pull request introduces several changes to the `kubeit` project,
    primarily focusing on enhancing logging, adding Docker image support,
    and improving URI parsing. Below are the most significant changes:
    
    ### Logging Enhancements:
    * Added logging for errors during manifest generation in
    `generate_manifest.go` by integrating the `logger` package.
    
    ### Docker Image Support:
    * Introduced a new function `loadKubeitResourcesFromDockerImage` in
    `loader.go` to load resources from Docker images.
    * Added utility functions in `docker.go` to check for Docker image
    existence.
    
    ### URI Parsing Improvements:
    * Replaced the `utils.ParseSourceConfigURI` with
    `utils.SourceConfigUriParser` to handle different URI schemes, including
    file and Docker image references.
    * Added `uri.go` for enhanced URI parsing and validation.
    
    ### Dependency Updates:
    * Updated `go.mod` to include new dependencies for Docker client and
    other necessary packages.
    [[1]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6R6-R7)
    [[2]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6R29)
    [[3]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6L41-R49)
    [[4]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6R105)
    
    ### Code Refactoring:
    * Renamed `LogResource` to `LogResources` and added detailed
    documentation for the function in `loader.go`.
    * Modified the `GenerateManifests` and `GenerateDockerLabels` functions
    in `main.go` to use the updated `LogResources` function.
    [[1]](diffhunk://#diff-c51d38b9ed61dab1971882842e91172df1a918a8c28be5768a7258f4504b7a17L32-R32)
    [[2]](diffhunk://#diff-c51d38b9ed61dab1971882842e91172df1a918a8c28be5768a7258f4504b7a17L56-R56)
    
    ---------
    
    Co-authored-by: Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>

---
+++ pkg/utils/docker.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/client"
+)
+
+// CheckDockerImageExists checks if a Docker image exists
+func CheckDockerImageExists(imageRef string) (bool, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return false, fmt.Errorf("failed to create Docker client: %w", err)
+	}
+
+	_, _, err = cli.ImageInspectWithRaw(context.Background(), imageRef)
+	if err != nil {
+		if client.IsErrNotFound(err) {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to inspect Docker image: %w", err)
+	}
+
+	return true, nil
+}
