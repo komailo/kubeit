cbebff1f1a9acc703e2a3a044321f96e4d622b85
Komail Kanjee <1796298+komailo@users.noreply.github.com>
2025-03-08 15:44:41 -0500

    feat: show deps versions in version cmd and license (#24)
    This pull request includes several updates and improvements to the
    `kubeit` project, including a license addition, dependency updates, and
    changes to the build configuration. The most important changes are
    summarized below:
    
    ### License Addition:
    * Added a BSD 3-Clause License to the
    `cmd/kubeit/commands/assets/LICENSE` file.
    
    ### Build Configuration Updates:
    * Modified the `main` entry in the `.goreleaser.yaml` file for the
    `kubeit-cli` build from `./cmd/main.go` to `./main.go`.
    * Changed the `formats` entry to `format` in the `archives` section of
    the `.goreleaser.yaml` file.
    
    ### Dependency Updates:
    * Updated various dependencies in the `go.mod` file to their latest
    versions, including `github.com/docker/docker`,
    `github.com/AdaLogics/go-fuzz-headers`, and others.
    [[1]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6L7-R7)
    [[2]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6L21-R26)
    [[3]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6L36-R76)
    [[4]](diffhunk://#diff-33ef32bf6c23acb95f5902d7097b7a1d5128ca061167ec0716715b0b9eeaa5f6L86-R163)
    
    ### Version Command Enhancements:
    * Enhanced the `version` command in `cmd/kubeit/commands/version.go` to
    include license information and display versions of dependencies.
    
    ### Version Change:
    * Updated the `Version` constant in `common/common.go` from `0.1.0` to
    `0.0.0-dev`.
    
    ---------
    
    Co-authored-by: Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>

--- cmd/kubeit/commands/version.go
+++ cmd/kubeit/commands/version.go
@@ -2,22 +2,60 @@ package commands
 
 import (
 	"fmt"
+	"runtime/debug"
+
+	_ "embed" // Ensure the embed package is imported
 
 	"github.com/komailo/kubeit/common"
+	"github.com/komailo/kubeit/internal/logger"
 	"github.com/spf13/cobra"
 )
 
-// Version of KubeIt (replace with actual versioning logic)
+//go:embed assets/LICENSE
+var licenseContent string
+
+var showLicense bool
+
+const (
+	licenseFlag = "license"
+)
+
+var depNames = map[string]string{
+	"github.com/docker/docker": "Docker Client",
+	"helm.sh/helm/v3":          "Helm",
+	"k8s.io/client-go":         "Kubernetes API machinery",
+}
 
 // VersionCmd provides the version of the tool
 var VersionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version of Kubeit",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Kubeit version: %s\n", common.Version)
+		if showLicense {
+			fmt.Println(licenseContent)
+		}
+		bi, ok := debug.ReadBuildInfo()
+		if !ok {
+			logger.Warnf("Failed to read build info")
+			return
+		}
+
+		// Iterate over the dependencies and print their versions
+		for _, dep := range bi.Deps {
+			if name, ok := depNames[dep.Path]; ok {
+				fmt.Printf("%s Version: %s\n", name, dep.Version)
+			}
+		}
+		fmt.Printf("Kubeit version: %s\n\n", common.Version)
+
+		if !showLicense {
+			fmt.Printf("For license information, run: kubeit version --%s\n", licenseFlag)
+		}
+
 	},
 }
 
 func init() {
-	// Ensure this command gets registered in the root command
+	VersionCmd.PersistentFlags().BoolVar(&showLicense, licenseFlag, false, "Print the license information")
+
 }
